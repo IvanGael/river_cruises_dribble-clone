51109baa0cdeab9e7232e8a52243d3c39619e511
Ivan <apedoivan@gmail.com>
2024-05-23 20:29:50 +0200

    little change

--- lib/ui/map_screen.dart
+++ lib/ui/map_screen.dart
@@ -148,3 +148,171 @@ class _MapScreenState extends State<MapScreen> {
     });
   }
 }
+
+
+
+
+// class _MapScreenState extends State<MapScreen> {
+//   String GOOGLE_MAPS_API_KEY = "AIzaSyDp0YPClQ7N4h2sOzuzChSmMWajpUC0OIo";
+//   final Location _locationController = Location();
+
+//   final Completer<GoogleMapController> _mapController = Completer<GoogleMapController>();
+
+//   static const LatLng _pGooglePlex = LatLng(37.4223, -122.0848);
+//   static const LatLng _pApplePark = LatLng(37.3346, -122.0090);
+//   LatLng? _currentP;
+
+//   Map<PolylineId, Polyline> polylines = {};
+//   Marker? _carMarker;
+//   BitmapDescriptor? _carIcon;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     initializeLocationTracking();
+//     _loadCarIcon();
+//   }
+
+//   Future<void> _loadCarIcon() async {
+//     _carIcon = await BitmapDescriptor.fromAssetImage(
+//       const ImageConfiguration(size: Size(8, 8)),
+//       'assets/images/car.png',
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: _currentP == null
+//           ? const Center(
+//               child: CircularProgressIndicator(strokeWidth: 7,),
+//             )
+//           : GoogleMap(
+//               onMapCreated: (GoogleMapController controller) {
+//                 _mapController.complete(controller);
+//                 if (_currentP != null) {
+//                   _cameraToPosition(_currentP!);
+//                 }
+//               },
+//               initialCameraPosition: const CameraPosition(
+//                 target: _pGooglePlex,
+//                 zoom: 6,
+//               ),
+//               markers: _buildMarkers(),
+//               polylines: Set<Polyline>.of(polylines.values),
+//             ),
+//     );
+//   }
+
+//   Future<void> _cameraToPosition(LatLng pos) async {
+//     final GoogleMapController controller = await _mapController.future;
+//     CameraPosition newCameraPosition = CameraPosition(
+//       target: pos,
+//       zoom: 45, 
+//     );
+//     await controller.animateCamera(
+//       CameraUpdate.newCameraPosition(newCameraPosition),
+//     );
+//   }
+
+//   Future<void> initializeLocationTracking() async {
+//     bool serviceEnabled;
+//     PermissionStatus permissionGranted;
+
+//     serviceEnabled = await _locationController.serviceEnabled();
+//     if (!serviceEnabled) {
+//       serviceEnabled = await _locationController.requestService();
+//       if (!serviceEnabled) {
+//         return;
+//       }
+//     }
+
+//     permissionGranted = await _locationController.hasPermission();
+//     if (permissionGranted == PermissionStatus.denied) {
+//       permissionGranted = await _locationController.requestPermission();
+//       if (permissionGranted != PermissionStatus.granted) {
+//         return;
+//       }
+//     }
+
+//     getLocationUpdates();
+//     getPolylinePoints().then((coordinates) {
+//       generatePolyLineFromPoints(coordinates);
+//     });
+//   }
+
+//   void getLocationUpdates() {
+//     _locationController.onLocationChanged.listen((LocationData currentLocation) {
+//       if (currentLocation.latitude != null && currentLocation.longitude != null) {
+//         setState(() {
+//           _currentP = LatLng(currentLocation.latitude!, currentLocation.longitude!);
+//           _updateCarMarker(_currentP!);
+//           _cameraToPosition(_currentP!);
+//         });
+//       }
+//     });
+//   }
+
+//   void _updateCarMarker(LatLng position) {
+//     if (_carIcon != null) {
+//       _carMarker = Marker(
+//         markerId: const MarkerId("_currentLocation"),
+//         icon: _carIcon!,
+//         position: position,
+//       );
+//     }
+//   }
+
+//   Set<Marker> _buildMarkers() {
+//     Set<Marker> markers = {
+//       const Marker(
+//         markerId: MarkerId("_sourceLocation"),
+//         icon: BitmapDescriptor.defaultMarker,
+//         position: _pGooglePlex,
+//       ),
+//       const Marker(
+//         markerId: MarkerId("_destionationLocation"),
+//         icon: BitmapDescriptor.defaultMarker,
+//         position: _pApplePark,
+//       )
+//     };
+
+//     if (_carMarker != null) {
+//       markers.add(_carMarker!);
+//     }
+
+//     return markers;
+//   }
+
+//   Future<List<LatLng>> getPolylinePoints() async {
+//     List<LatLng> polylineCoordinates = [];
+//     PolylinePoints polylinePoints = PolylinePoints();
+//     PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+//       GOOGLE_MAPS_API_KEY,
+//       PointLatLng(_pGooglePlex.latitude, _pGooglePlex.longitude),
+//       PointLatLng(_pApplePark.latitude, _pApplePark.longitude),
+//       travelMode: TravelMode.driving,
+//     );
+//     if (result.points.isNotEmpty) {
+//       for (var point in result.points) {
+//         polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+//       }
+//     } else {
+//       debugPrint(result.errorMessage);
+//     }
+//     return polylineCoordinates;
+//   }
+
+//   void generatePolyLineFromPoints(List<LatLng> polylineCoordinates) {
+//     PolylineId id = const PolylineId("poly");
+//     Polyline polyline = Polyline(
+//       polylineId: id,
+//       color: AppColors.primary,
+//       points: polylineCoordinates,
+//       width: 8,
+//     );
+//     setState(() {
+//       polylines[id] = polyline;
+//     });
+//   }
+// }
